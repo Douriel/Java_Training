4f19d5a577e39a4638a6d0a8212453c2194491e0
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2022-10-10 13:22:34 +0200

    Fist small proyect

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author shak_
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        List names = new ArrayList();
+        
+        System.out.println("Hello World!");
+    }
+}
