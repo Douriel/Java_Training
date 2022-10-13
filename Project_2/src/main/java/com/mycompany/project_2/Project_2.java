93f138984036445ba08142d83f3f38dbc024690a
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2022-10-13 11:59:45 +0100

    working from IntelliJ
    I have add some lines of code on the second project, I still do not see which is the difference between call Object and T object

--- Project_2/src/main/java/com/mycompany/project_2/Project_2.java
+++ Project_2/src/main/java/com/mycompany/project_2/Project_2.java
@@ -5,13 +5,37 @@
 
 package com.mycompany.project_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author shak_
  */
+
 public class Project_2 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static Character[] characters = {'L', 'u', 'i', 's'};
+    static Integer[] integers = {1, 2, 3, 4};
+    static Boolean[] booleans = {true, false, true};
+
+    public static List arrayToList(Object [] array, List<Object> list){
+        for(Object object: array){
+            list.add(object);
+        }
+        return list;
     }
+
+    public static void main(String[] args) {
+        List<Character> charList = arrayToList(characters, new ArrayList<>());
+        List<Integer> intList = arrayToList(integers, new ArrayList<>());
+        List<Boolean> boolList = arrayToList(booleans, new ArrayList<>());
+
+        System.out.println(charList.get(0));
+        System.out.println(intList.get(0));
+        System.out.println(boolList.get(0));
+
+    }
+
+
 }
