06cd8c8b7f2d63b962fba0ad48b5b40b4f4b66bf
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2022-10-19 18:07:17 +0100

    ParalleStream
    Faster filter

--- StreamInJava/src/Main.java
+++ StreamInJava/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Book> books = populateLibrary();
 
-        books.stream().filter(book -> {
+        books.parallelStream().filter(book -> {
             return book.getAuthor().startsWith("J");
         }).filter(book -> {
             return book.getTitle().startsWith("L");
