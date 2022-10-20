3d53babcc380b991f7adcd5f11afeb51114c0e67
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2022-10-20 15:20:02 +0100

    Creating packages

---
+++ HelloWold/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // I really want to create a new package editable
+        System.out.println("Hello world!");
+    }
+}
