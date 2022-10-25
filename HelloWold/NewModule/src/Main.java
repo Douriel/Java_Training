9adbaa66b725679c321ae86ebcf6a2d5eb7fdfdc
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2022-10-25 16:35:48 +0100

    Thread example
    ThreadExample class extends from thread.

--- HelloWold/NewModule/src/Main.java
+++ HelloWold/NewModule/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-
-        // I really want to create a new Java module info
         System.out.println("Hello world!");
     }
 }
